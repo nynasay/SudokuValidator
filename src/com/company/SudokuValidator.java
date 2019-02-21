6d3315127920df20634b45215c92608f0fa7b085
Nyna Sayarath <nsayarath@gmail.com>
2019-02-21 00:13:33 -0500

    tried to make threads, haven't compiled yet

--- src/com/company/SudokuValidator.java
+++ src/com/company/SudokuValidator.java
@@ -16,71 +16,100 @@ public class SudokuValidator {
                 {4, 9, 6, 1, 8, 2, 5, 7, 3},
                 {2, 8, 5, 4, 7, 3, 9, 1, 6}};
 
+        ValidateColumn columns = new ValidateColumn(correctSudoku);
+        ValidateRow rows = new ValidateRow(correctSudoku);
+        ValidateGrid three_by_three = new ValidateGrid(correctSudoku);
 
         long start = System.nanoTime();
         long end = System.nanoTime();
 
         DecimalFormat format = new DecimalFormat("#.##");
         //boolean valid = true;
-        boolean c = threadCreator(correctSudoku);
+
 
     }
 
-    public static boolean threadCreator(int sudoku[][]) {
-
-        int [] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int [] eval = new int[9];
-
-        boolean valid = true;
-
-
-        //Looping through all of the columns
-        for(int row = 0; row < sudoku.length; row++) {
-            for(int col = 0; col < sudoku[0].length; col++) {
-                eval[col] = sudoku[row][col];
-            }
-            Arrays.sort(eval);
-            if (!Arrays.equals(values, eval)) {
-                valid = false;
-            }
-        }
-        Arrays.fill(eval, 0);
-
-        //Looping through all the rows
-        for(int col = 0; col < sudoku[0].length; col++) {
+    static class ValidateColumn implements Runnable {
+        public ValidateColumn(int sudoku[][]) {
+            int [] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int [] eval = new int[9];
+            boolean valid = true;
+            //Looping through all of the columns
             for(int row = 0; row < sudoku.length; row++) {
-                eval[row] = sudoku[row][col];
-            }
-            Arrays.sort(eval);
-            if (!Arrays.equals(values, eval)) {
-                valid = false;
-            }
-        }
-        Arrays.fill(eval, 0);
-
-
-        //Looping through all the boxes
-        for (int i =0; i<3; i++) {
-            for(int j = 0; j <3;j++) {
-                int k =0;
-                eval = new int [9];
-                for( int row = i*3; row <(i*3)+3; row++) {
-                    for( int column = j *3; column< (i*3)+3; column++) {
-                        eval[k] = sudoku[row][column];
-                        System.out.println("k: " + k);
-                        k++;
-
-                    }
-
+                for(int col = 0; col < sudoku[0].length; col++) {
+                    eval[col] = sudoku[row][col];
+                }
+                Arrays.sort(eval);
+                if (!Arrays.equals(values, eval)) {
+                    valid = false;
                 }
             }
+            Arrays.fill(eval, 0);
+            Arrays.sort(eval);
+            if (!Arrays.equals(values, eval)) {
+                valid = false;
+            }
         }
-        Arrays.sort(eval);
-        if (!Arrays.equals(values, eval)) {
-            valid = false;
+        public void run(){
         }
 
-
-        return valid;
     }
+
+    static class ValidateRow implements Runnable {
+        public ValidateRow(int sudoku[][]) {
+            int [] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int [] eval = new int[9];
+            boolean valid = true;
+            for(int col = 0; col < sudoku[0].length; col++) {
+                for(int row = 0; row < sudoku.length; row++) {
+                    eval[row] = sudoku[row][col];
+                }
+                Arrays.sort(eval);
+                if (!Arrays.equals(values, eval)) {
+                    valid = false;
+                }
+            }
+            Arrays.fill(eval, 0);
+            Arrays.sort(eval);
+            if (!Arrays.equals(values, eval)) {
+                valid = false;
+            }
+        }
+        public void run(){
+        }
+
+    }
+
+    static class ValidateGrid implements Runnable {
+        public ValidateGrid(int sudoku[][]) {
+            int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int[] eval = new int[9];
+            boolean valid = true;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int k = 0;
+                    eval = new int[9];
+                    for (int row = i * 3; row < (i * 3) + 3; row++) {
+                        for (int column = j * 3; column < (i * 3) + 3; column++) {
+                            eval[k] = sudoku[row][column];
+                            System.out.println("k: " + k);
+                            k++;
+
+                        }
+
+                    }
+                }
+
+            }
+            Arrays.fill(eval, 0);
+            Arrays.sort(eval);
+            if (!Arrays.equals(values, eval)) {
+                valid = false;
+            }
+        }
+        public void run(){
+        }
+
+    }
+
 }
