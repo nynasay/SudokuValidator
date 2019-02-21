d8d03a6ef54f81a60c1a15fdd367e33f6f7360e3
Nyna Sayarath <nsayarath@gmail.com>
2019-02-21 00:21:27 -0500

    made small changes

--- src/com/company/SudokuValidator.java
+++ src/com/company/SudokuValidator.java
@@ -6,7 +6,7 @@ public class SudokuValidator {
 
     public static void main(String[] args) {
 
-        int correctSudoku[][]= {{6, 2, 4, 5, 3, 9, 1, 8, 7},
+        int correctSudoku[][] = {{6, 2, 4, 5, 3, 9, 1, 8, 7},
                 {5, 1, 9, 7, 2, 8, 6, 3, 4},
                 {8, 3, 7, 6, 1, 4, 2, 9, 5},
                 {1, 4, 3, 8, 6, 5, 7, 2, 9},
@@ -16,27 +16,42 @@ public class SudokuValidator {
                 {4, 9, 6, 1, 8, 2, 5, 7, 3},
                 {2, 8, 5, 4, 7, 3, 9, 1, 6}};
 
+        //this method is to just print the sudoko
+        for (int[] row : correctSudoku) {
+            printRow(row);
+        }
+
+
         ValidateColumn columns = new ValidateColumn(correctSudoku);
         ValidateRow rows = new ValidateRow(correctSudoku);
         ValidateGrid three_by_three = new ValidateGrid(correctSudoku);
 
-        long start = System.nanoTime();
-        long end = System.nanoTime();
+        //long start = System.nanoTime();
+        //long end = System.nanoTime();
 
-        DecimalFormat format = new DecimalFormat("#.##");
+        //DecimalFormat format = new DecimalFormat("#.##");
         //boolean valid = true;
 
 
     }
 
+    //this method is just to print the table
+    public static void printRow(int[] row) {
+        for (int i : row) {
+            System.out.print(i);
+            System.out.print("\t");
+        }
+        System.out.println();
+    }
+
     static class ValidateColumn implements Runnable {
         public ValidateColumn(int sudoku[][]) {
-            int [] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            int [] eval = new int[9];
+            int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int[] eval = new int[9];
             boolean valid = true;
             //Looping through all of the columns
-            for(int row = 0; row < sudoku.length; row++) {
-                for(int col = 0; col < sudoku[0].length; col++) {
+            for (int row = 0; row < sudoku.length; row++) {
+                for (int col = 0; col < sudoku[0].length; col++) {
                     eval[col] = sudoku[row][col];
                 }
                 Arrays.sort(eval);
@@ -50,18 +65,19 @@ public class SudokuValidator {
                 valid = false;
             }
         }
-        public void run(){
+
+        public void run() {
         }
 
     }
 
     static class ValidateRow implements Runnable {
         public ValidateRow(int sudoku[][]) {
-            int [] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            int [] eval = new int[9];
+            int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int[] eval = new int[9];
             boolean valid = true;
-            for(int col = 0; col < sudoku[0].length; col++) {
-                for(int row = 0; row < sudoku.length; row++) {
+            for (int col = 0; col < sudoku[0].length; col++) {
+                for (int row = 0; row < sudoku.length; row++) {
                     eval[row] = sudoku[row][col];
                 }
                 Arrays.sort(eval);
@@ -75,7 +91,8 @@ public class SudokuValidator {
                 valid = false;
             }
         }
-        public void run(){
+
+        public void run() {
         }
 
     }
@@ -107,7 +124,8 @@ public class SudokuValidator {
                 valid = false;
             }
         }
-        public void run(){
+
+        public void run() {
         }
 
     }
